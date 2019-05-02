06c9230fba308b52fb2ce59966be667e7b4229b4
Houng <majinht@hotmail.com>
2019-05-02 16:29:34 +0200

    woo

--- js/app.js
+++ js/app.js
@@ -31,3 +31,4 @@ function run($rootScope, $location, $cookies, $http) {
         }
     })
 }
+
--- js/controllers.js
+++ js/controllers.js
@@ -1,23 +1,21 @@
-app.controller("cardController", function($scope, productService, $http) {
-    productService.itemList().then(function (data) { 
-        $scope.itemList = data;
-        $scope.rowLimit = {
-        "type": "select",
-        "name": "rowLimit",
-        "value": "5",
-        "values": ["5","10","20", "50", "100"]
-    };
+app.controller("cardController", function ($scope, $http) {
 
+    
     $http.get("http://localhost:3001/api/items")
-    .then((res) => $scope.itemList = res.data)
+    .then(function(res) {
+        $scope.itemList = res.data; 
+    });
+
+    $scope.showProducts = "5";
 
     $scope.sortItem = "";
 
-    $scope.search = function (item) {
-        if ($scope.searchText == undefined) {
+    // filter search
+    $scope.search = function(item) {
+        if($scope.searchText == undefined) {
             return true;
         } else {
-            if (
+            if(
                 item.productname.toLowerCase().indexOf($scope.searchText.toLowerCase()) != -1
             ) {
                 return true;
@@ -26,17 +24,14 @@ app.controller("cardController", function($scope, productService, $http) {
         return false;
     };
 
-
+    // RatingCalculator
     const starsTotal = 5;
 
-    $scope.rating = function (input) {
+    $scope.rating = function(input) {
         const starProcentage = (input / starsTotal) * 100;
-        const starProcentageRounded = `${Math.round(starProcentage / 10) * 10}%`;
+        const starProcentageRounded = `${Math.round(starProcentage /10) * 10}%`;
 
         return starProcentageRounded;
     }
-
-
     
-    })
-})
+});
